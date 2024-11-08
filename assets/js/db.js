24d2b173092344319ce388b318a7c3b8d0ec7eae
HoangNguyen005 <nguyentronghoang205@gmail.com>
2024-11-08 21:57:13 +0700

    Create audi tab & product section

---
+++ assets/js/db.js
@@ -0,0 +1,197 @@
+export const object = {
+    audiCars: [
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',      
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',   
+        }
+    ],
+    bmwCars: [
+        {
+            image: '../assets/images/car/BMW/2024-BMW-520i-1.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/BMW/2024-BMW-520i-1.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',   
+        }
+    ],
+    vinfastCars: [
+        {
+            image: './assets/images/car/Audi/Audi PB18 e-tron Car Model.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/BMW/2024-BMW-520i-1.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',        
+        },
+        {
+            image: '../assets/images/car/Audi/Audi R8 Coupé V10 GT RWD.jpg',
+            name: 'Audi R8 coupe v10 GT RWD',
+            price: 3000,
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus tempora aperiam corrupti, quam quo voluptate ratione asperiores cumque architecto. Corrupti unde, repellat deleniti ipsum ipsa eos voluptas magnam rerum aliquid aliquam, quidem autem earum iusto?',
+            fuel: 'xăng',
+            status: 'Mới',   
+        }
+    ],
+    hotCars: [
+        {
+            name: 'Audi R8 Coupe car model',
+            image: './assets/images/car/Audi/Audi-R8-Coupe-car-model.jpg',
+            price: 2300,
+            km: 6500,
+            brand: 'audi'
+        },
+        {
+            name: 'BMW 2024 i5 eDrive 40 M sport',
+            image: '../assets/images/car/BMW/BMW 2024 i5 eDrive 40 M sport.jpeg',
+            price: 2300,
+            km: 6500,
+            brand: 'bmw'
+        },
+        {
+            name: 'BMW-520i',
+            image: '../assets/images/car/BMW/2024-BMW-520i-1.jpg',
+            price: 2300,
+            km: 6500,
+            brand: ''
+        },
+        {
+            name: 'Vinfast vf9',
+            image: '../assets/images/car/Vinfast/Vinfast-vf9.jpeg',
+            price: 2300,
+            km: 6500,
+            brand: 'Vinfast'
+        },
+        {
+            name: 'audi rs e tron gt highlighted',
+            image: '../assets/images/car/Audi/883x883-audi-rs-e-tron-gt-highlighted-car-teaser.jpg',
+            price: 2300,
+            km: 6500,
+            brand: ''
+        },
+        {
+            name: 'Vinfast vf3',
+            image: '../assets/images/car/Vinfast/VinFast-VF3.jpg',
+            price: 2300,
+            km: 6500,
+            brand: ''
+        },
+        
+    ]
+}
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+import { object } from "./db.js"
+
+;(function app() {
+    renderCars()
+})()
+
+function renderCars() {
+    const carsList = document.querySelector('.car-list')
+    const html = object.hotCars.map(carInfo => {
+        return `
+          <div class="col col-sm-12 col-md-6 col-lg-4">
+                         <div class="car-item">
+                            <div class="car-item-img">
+                                <img src="${carInfo.image}" alt="">
+                                <i class="cart-icon fa-solid fa-cart-plus"></i>
+                            </div>
+                            <div class="car-item-content">
+                                <h2 class="car-name">${carInfo.name}</h2>
+                                <div class="car-parameter">
+                                    <div class="parameter">
+                                        <i class="fa-solid fa-gear"></i>
+                                        <p class="parameter-name">automatic</p>
+                                    </div>
+                                    <div class="parameter">
+                                        <i class="fa-solid fa-gas-pump"></i>
+                                        <p class="parameter-name">diesel</p>
+                                    </div>
+                                    <div class="parameter">
+                                        <i style="color: rgb(255, 106, 0)" class="fa-solid fa-road"></i>
+                                        <p class="parameter-name">${carInfo.km}km</p>
+                                    </div>
+                                </div>
+                                <hr style="width: calc(100% + 30px); transform: translateX(-15px); border-color: #f8f8f8">
+                                <div class="car-item-bottom">
+                                    <div class="car-item-rating">
+                                        <i class="fa-regular fa-star"></i>
+                                        <i class="fa-regular fa-star"></i>
+                                        <i class="fa-regular fa-star"></i>
+                                        <i class="fa-regular fa-star"></i>
+                                        <i class="fa-regular fa-star"></i>
+                                    </div>
+                                    <div class="car-item-price">
+                                        <span>Giá: </span>
+                                        <span class="price">${carInfo.price}</span>
+                                        <span>/mo</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `
+    })
+    carsList.innerHTML = html.join('')
+}
